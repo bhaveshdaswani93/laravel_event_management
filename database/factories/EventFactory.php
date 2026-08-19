76719a031379507e6df7d241ac3afe66be6d0fd8
Bhavesh <bhaveshdaswani93@gmail.com>
2026-08-19 08:37:51 +0000

    create event model. factory, migration

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Event;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Event>
+ */
+class EventFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(3),
+            'description' => fake()->paragraph(),
+            'location' => fake()->address(),
+            'starts_at' => fake()->dateTimeBetween('now', '+6 months'),
+            'capacity' => fake()->numberBetween(10, 500),
+        ];
+    }
+}
